5e1a99c93efd07ebf453a8be042d3a336acc7a8f
arkan dzaky <16521230@mahasiswa.itb.ac.id>
2022-04-14 21:00:17 +0700

    revisi-1 list & buy

--- buy_game.py
+++ buy_game.py
@@ -1,14 +1,9 @@
-# import utility as ul
+from utility import length
+from time import gmtime, strftime
 
 # Contoh data
 # d = [[[['id', '1'], ['username', 'admin'], ['nama', 'Admin'], ['password', 'bklzo2584>'], ['role', 'user'], ['saldo', '100000']], [['id', '2'], ['username', 'tara123'], ['nama', 'Tara'], ['password', 'bazbobtabokn'], ['role', 'user'], ['saldo', '50000']]], [[['id', 'GAME001'], ['nama', 'Python Gaming'], ['kategori', 'Adventure'], ['tahun_rilis', '2022'], ['harga', '150000'], ['stok', '100']]], [[['game_id', 'GAME001'], ['nama', 'Valorant'], ['harga', '0'], ['user_id', '1'], ['tahun_beli', '2021']]], [[['game_id', 'GAME001'], ['user_id', '1']]]]
 
-
-def length(arr) :
-    length = 0
-    for i in arr :
-        length += 1
-    return length
 
 def stock(a, data) :
     stock = data[1][a][5][1]   # dari data game
@@ -22,16 +17,20 @@ def saldo(a, data) :
     saldo = data[0][a][5][1]   # dari data user
     return int(saldo)
 
+def nama_game(a, data) :
+    nama_game = data[1][a][1][1]     # dari data game
+    return nama_game
+
 def buy_game(userID, d) :
     user = d[0]
     game = d[1]
-    # riwayat = d[2]
+    riwayat = d[2]
     milik = d[3]
 
     gameID = str(input('Masukkan ID Game: '))
 
     for i in range(length(game)) :  # cari gameID di game
-        if gameID in game[i][0] :   
+        if gameID in game[i][0] :   # jika ada gameID di game
             if stock(i, d) > 0 :       # jika stok > 0
                 tidak_punya_game = True
                 for j in range(length(milik)) :                             # cari gameID dan userID di milik
@@ -48,20 +47,24 @@ def buy_game(userID, d) :
                                 print('Saldo Anda sekarang adalah Rp' + str(int(saldo(k, d)) - int(price(i, d))) + '.') # saldo baru
                                 
                                 user[k][5][1] = str(int(saldo(k, d)) - int(price(i, d)))                        # update saldo
-                                game[i][5][1] = str(int(stock(i, d)) - 1)                                    # update stok
-                                milik + [[['game_id', gameID], ['user_id', userID]]]                      # update kepemilikan
+                                game[i][5][1] = str(int(stock(i, d)) - 1)                                       # update stok
+                                milik + [[['game_id', gameID], ['user_id', userID]]]                            # update kepemilikan
+                                riwayat + [[['game_id', gameID], ['nama', str(nama_game(i, d))], ['harga', str(price(i, d))], ['user_id', userID], ['tahun_beli', strftime("%Y", gmtime())]]] # update riwayat
                                 break
                             else :
-                                print('Saldo Anda tidak cukup.')                                            # saldo tidak cukup
+                                print('Saldo Anda tidak cukup.')            # saldo tidak cukup
                                 break
-    # KETERANGAN :
-    # i : index game
-    # j : index milik / kepemilikan
-    # k : index user
+        # KETERANGAN :
+        # i : index game
+        # j : index milik / kepemilikan
+        # k : index user
 
             else :  # jika stok = 0
                 print('Mohon maaf, stok Game "%s" sedang habis.' % game[i][1][1])               
             break
+        else :  # jika tidak ada game dengan gameID yang diinput
+            print('Mohon maaf, game tidak ditemukan.')
+            print('Coba periksa kembali masukan Anda. Jika menurut Anda terjadi kesalahan, silakan hubungi admin.')
 
 
 # -- TEST --
